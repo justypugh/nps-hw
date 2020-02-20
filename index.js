4dc3066bf7aa03bd0a80b28cebedd604442a7c0d
Justin Pugh <justypugh@icloud.com>
2020-02-19 16:28:37 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const apiKey = 'ZqTk40zhb5FFd26wfUY80MBLeNA2TZ2Ug1XEB5Uq';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+return queryItems.join('&');
+}
+
+function getParks(query, maxResults=10) {
+    const params = {
+        stateCode: [query],
+        limit: maxResults,
+        api_key: apiKey,
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+
+    console.log(url);
+
+    fetch(url)
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => {
+        $("#js-error-message").text(`Something went wrong: ${error.message}`);
+    });
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $("#results-list").empty();
+    for(let i = 0; i < responseJson.data.length; i++) {
+        $("#results-list").append(`
+            <li>
+                <h2>${responseJson.data[i].fullName}</h2>
+                <p>${responseJson.data[i].description}</p>
+                <a target='_blank' href='${responseJson.data[i].url}'>Visit the website here!</a>
+            </li>
+        `)};
+    $("#results").removeClass("hidden");    
+};
+
+
+function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      const searchTerm = $('#js-search-term').val();
+      const maxResults = $('#js-max-results').val();
+      getParks(searchTerm, maxResults);
+    });
+  }
+  
+  $(watchForm);
